8fef915299f660dc30646cfd72107d4f596ffae7
Pedro Martins <pedrogmhead@gmail.com>
2019-07-01 13:01:32 -0300

    added smooth scrooling
    all buttons are now smooth

--- index.php
+++ index.php
@@ -69,15 +69,15 @@
 				<ul id="menubar" class="navbar-nav nav collapse navbar-collapse justify-content-end">
 
 					<li class="nav-item">
-						<a href="#cardapio" class="nav-link mx-2">Cardápio</a>
+						<a href="#" onclick="go('#cardapio')" class="nav-link mx-2">Cardápio</a>
 					</li>
 
 					<li class="nav-item">
-						<a href="#infos" class="nav-link mx-2">Informações</a>
+						<a href="#" onclick="go('#infos')" class="nav-link mx-2">Informações</a>
 					</li>
 
 					<li class="nav-item">
-						<a href="#encontre" class="nav-link mx-2">Nos encontre</a>
+						<a href="#" onclick="go('#encontre')" class="nav-link mx-2">Nos encontre</a>
 					</li>
 					
 				</ul>
@@ -302,9 +302,7 @@
 
 				</div>
 
-				<div class="scrolltop">
-					<a href="#home"><i class="fas fa-arrow-up"></i></a>
-				</div>
+				<button id="arrow" type="button" onclick="go('#home')" class="btn btn-outline-light"><i class="fas fa-arrow-up"></i></button>
 
 				<div class="col-12 text-center">			
 					<hr>
@@ -321,6 +319,12 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+	<script>
+		const go = (elem) => {
+			window.scroll({top: document.querySelector( elem ).offsetTop, left: 0, behavior: 'smooth'});
+		}	
+	</script>
 	
 </body>
 
