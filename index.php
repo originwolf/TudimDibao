8e38dec8f720e7d53774f9717cf9f38cc408c8cd
Pedro Martins <pedrogmhead@gmail.com>
2019-07-01 13:28:34 -0300

    facebook widget moved
    script moved to the start of the body tag

--- index.php
+++ index.php
@@ -24,6 +24,8 @@
 <body>
 
 	<div id="fb-root"></div>
+	<!-- Script necessário ao widget do facebook -->
+	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v3.3"></script>
 
 	<div class="container-fluid">
 
@@ -304,9 +306,6 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-	
-	<!-- Script necessário ao widget do facebook -->
-	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v3.3"></script>
 	
 	<!-- Scripts necessários para o funcionamento interno da página -->
 	<script>
